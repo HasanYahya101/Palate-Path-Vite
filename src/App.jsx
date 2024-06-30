703e127c631f7f067422e543f7e616d6e77deb75
Hasan Yahya <hasna786yahya@gmail.com>
2024-07-01 01:38:54 +0500

    feat: Add responsive behavior to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Playground } from './components/component/playground';
+import Apology from './components/component/apology';
+
+const isDesktop = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    const isMobile = /mobile|android|iphone|ipad|tablet|touch|samsung|fridge/i.test(userAgent);
+    const isSmallScreen = window.innerWidth <= 1024;
+    return !isMobile && !isSmallScreen;
+};
 
 function App() {
-    return (
-        <Playground />
-    )
+    const [isDesktopView, setIsDesktopView] = React.useState(isDesktop());
+    if (!isDesktopView) {
+        return <Apology />;
+    }
+    else {
+        return (
+            <Playground />
+        )
+    }
 }
 
 export default App
